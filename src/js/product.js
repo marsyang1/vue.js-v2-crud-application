4ab000a0d7c07bc9f2570499116b5b4862052f3c
yangyuchi <marsyang1@gmail.com>
2017-06-17 13:48:09 +0800

    add webpack

---
+++ src/js/product.js
@@ -0,0 +1,92 @@
+var products = [
+  {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
+  {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
+  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
+];
+
+function findProduct (productId) {
+  return products[findProductKey(productId)];
+};
+
+function findProductKey (productId) {
+  for (var key = 0; key < products.length; key++) {
+    if (products[key].id == productId) {
+      return key;
+    }
+  }
+};
+
+export var ProductList = Vue.extend({
+  template: '#product-list',
+  data: function () {
+    return {products: products, searchKey: ''};
+  },
+  computed : {
+    filteredProducts: function () {
+    var self = this;
+    console.log()
+    return self.products.filter(function (product) {
+      return product.name.indexOf(self.searchKey) !== -1
+    })
+  }
+}
+});
+
+export var Product = Vue.extend({
+  template: '#product',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  }
+});
+
+export var ProductEdit = Vue.extend({
+  template: '#product-edit',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    updateProduct: function () {
+      var product = this.product;
+      products[findProductKey(product.id)] = {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price
+      };
+      router.push('/');
+    }
+  }
+});
+
+export var ProductDelete = Vue.extend({
+  template: '#product-delete',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    deleteProduct: function () {
+      products.splice(findProductKey(this.$route.params.product_id), 1);
+      router.push('/');
+    }
+  }
+});
+
+export var AddProduct = Vue.extend({
+  template: '#add-product',
+  data: function () {
+    return {product: {name: '', description: '', price: ''}
+    }
+  },
+  methods: {
+    createProduct: function() {
+      var product = this.product;
+      products.push({
+        id: Math.random().toString().split('.')[1],
+        name: product.name,
+        description: product.description,
+        price: product.price
+      });
+      router.push('/');
+    }
+  }
+});
